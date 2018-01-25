c49651313659df39350803a3a443975d4a9313b3
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2018-01-25 17:27:21 -0500

    Push dynamic mappings to elastic search.

--- run_filebeat.sh
+++ run_filebeat.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-docker run --rm -v $(pwd)/filebeat.yml:/usr/share/filebeat/filebeat.yml \
-				-v $(pwd)/logs_for_elk/extended_log:/logs_for_elk/extended_log \
-				--network=elk_default \
-				docker.elastic.co/beats/filebeat:6.1.1
+docker run --rm --name filebeat -v $(pwd)/filebeat.yml:/usr/share/filebeat/filebeat.yml:rw \
+                                -v $(pwd)/reactomews.dynamicmapping.json:/reactomews.dynamicmapping.json \
+                                -v /datastore/logs_for_elk/extended_log:/logs_for_elk/2016_extended_log \
+                                --network=elksetup_default \
+                                docker.elastic.co/beats/filebeat:6.1.1 /bin/bash -c "curl -XPUT 'esserver:9200/reactome-main?pretty' -H 'Content-Type: application/json' -d@/reactomews.dynamicmapping.json && curl -XPUT 'esserver:9200/reactomews?pretty' -H 'Content-Type: application/json' -d@/reactomews.dynamicmapping.json && /usr/local/bin/docker-entrypoint -e"
