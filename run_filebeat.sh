8bfeb9f265cb4fd39cd0ad9b370a69b38ff0ab7e
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2019-02-12 15:06:09 -0500

    Removed POST of mapping
    ...that should be done manually, before attempting to import any logs.

--- run_filebeat.sh
+++ run_filebeat.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
+# Note: You will probably need to update the mountpoints for the log files.
+# Also: Make sure you `POST _template/reactome_template` the content of reactomews_dynamicmapping.json BEFORE
+# trying to import any log files.
+
 docker run --rm --name filebeat -v $(pwd)/filebeat.yml:/usr/share/filebeat/filebeat.yml:rw \
                                 -v $(pwd)/reactomews.dynamicmapping.json:/reactomews.dynamicmapping.json \
                                 -v /datastore/logs_for_elk/extended_log:/logs_for_elk/2016_extended_log \
                                 --network=elksetup_default \
-                                docker.elastic.co/beats/filebeat:6.6.0 /bin/bash -c "curl -XPUT 'esserver:9200/reactome-main?pretty' -H 'Content-Type: application/json' -d@/reactomews_dynamicmapping.json && curl -XPUT 'esserver:9200/reactomews?pretty' -H 'Content-Type: application/json' -d@/reactomews_dynamicmapping.json && /usr/local/bin/docker-entrypoint -e"
+                                docker.elastic.co/beats/filebeat:6.6.0 /bin/bash -c "/usr/local/bin/docker-entrypoint -e"
