9c94ecc365c047061895fd010d56961fb9afa9f2
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2019-02-12 13:15:59 -0500

    generate random password

--- create_certs.sh
+++ create_certs.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-
-openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
-openssl rsa -passin pass:x -in server.pass.key -out server.key
+PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+openssl genrsa -des3 -passout pass:$PASS -out server.pass.key 2048
+openssl rsa -passin pass:$PASS -in server.pass.key -out server.key
 openssl req -new -key server.key -out server.csr
 openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
 
