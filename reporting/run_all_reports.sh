b8f04252bee1450d04958c8764be711ebbf4c291
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2019-10-17 16:28:27 -0400

    Fixed name of query file.

--- reporting/run_all_reports.sh
+++ reporting/run_all_reports.sh
@@ -15,7 +15,7 @@ MAIL_TO=$3
 
 bash run_report.sh $FROM_DATE $TO_DATE data_xfer_by_month_no_bots.query.json data_xfer_no_bots "Month, Gigabytes transferred" '.aggregations.Month.buckets[] | [ .key_as_string, .Bytes_transferred.value ] | @csv' $MAIL_TO
 
-bash run_report.sh $FROM_DATE $TO_DATE ip_counts_by_month.query.json ip_counts_by_month "Month, IP Count" '.aggregations.month_agg.buckets[] | [ .key_as_string, .ip_count.value ] | @csv' $MAIL_TO
+bash run_report.sh $FROM_DATE $TO_DATE unique_ips_by_month.query.json ip_counts_by_month "Month, IP Count" '.aggregations.month_agg.buckets[] | [ .key_as_string, .ip_count.value ] | @csv' $MAIL_TO
 
 bash run_report.sh $FROM_DATE $TO_DATE hits_by_month.query.json hits_by_month "Month, Hits" '.aggregations.month.buckets[] | [ .key_as_string, .doc_count ] | @csv' $MAIL_TO
 
