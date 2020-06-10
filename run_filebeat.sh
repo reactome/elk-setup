dfbc936b2cd6d4a892a7503a18048324eb6009fd
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2020-06-10 17:45:31 -0400

    Sync version with other components.

--- run_filebeat.sh
+++ run_filebeat.sh
@@ -5,6 +5,6 @@
 # trying to import any log files.
 
 docker run --rm --name filebeat -v $(pwd)/filebeat.yml:/usr/share/filebeat/filebeat.yml:rw \
-                                -v $(pwd)/extended_log:/logs_for_elk/extended_log \
+                                -v $(pwd)/extended_log:/logs_for_elk/ \
                                 --network=elk_default \
-                                docker.elastic.co/beats/filebeat:7.4.0 /bin/bash -c "/usr/local/bin/docker-entrypoint -e"
+                                docker.elastic.co/beats/filebeat:7.7.0 /bin/bash -c "/usr/local/bin/docker-entrypoint -e"
